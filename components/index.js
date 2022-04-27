6bc06d4adb3e486b9ed2ce3d69aa4e967df10aca
Muhammad Abdullah Khan <abdullah@repairdesk.co>
2022-04-27 13:05:17 +0500

    first commit

---
+++ components/index.js
@@ -0,0 +1,30 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import {
+  SubInfo,
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+} from "./SubInfo";
+import { CircleButton, RectButton } from "./Button";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  DetailsBid,
+  DetailsDesc,
+};
